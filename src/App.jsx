484dc8c5d6058732b97204c4d1358eb30dcbe4e5
RazelRaz <razel16dcm@gmail.com>
2023-07-06 20:55:29 +0600

    conditioning inside jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 
 
 const App = () => {
+
+  let marks = 90;
+
   return (
     <div>
       <button className="btn btn-success">Im Button</button>
       <button className="btn btn-danger">Danger</button>
+      {
+        marks > 50 ? 'Passed' : 'Failed'
+      }
     </div>
   )
 }
