f418161ec02d4f3a5fefb055127e8e4eb83a5b83
RazelRaz <razel16dcm@gmail.com>
2023-07-06 20:20:16 +0600

    react folder structure

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+// bootstrap js
+import 'bootstrap';
+// bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
