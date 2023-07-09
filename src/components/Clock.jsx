5c72b1c6883e98d6a5a51859e0b2951eb4e0e98d
RazelRaz <razel16dcm@gmail.com>
2023-07-09 16:28:41 +0600

    JSX Loop Inside - map

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,9 +1,17 @@
 
 
 const Clock = ({ locale }) => {
+    let cities = ["Dhaka", "Pune", "Lahore", "Sydney"];
     return (
         <div>
             <p>current time: {new Date().toLocaleTimeString(locale)}</p>
+            <ul>
+                {
+                    cities.map((city, key) => {
+                        return <li key={key}>{city}</li>
+                    })
+                }
+            </ul>
         </div>
     );
 };
