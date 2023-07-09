5ef420ae2236d6e7ea93e31e7f13d5f900e8ecd0
RazelRaz <razel16dcm@gmail.com>
2023-07-09 16:21:42 +0600

    JSX Immediately invoked function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
 
 
 const Header = () => {
+    let marks = 98
     return (
         <div>
             <ul>
                 <li>Home</li>
                 <li>Contact</li>
             </ul>
+            {(
+                () => {
+                    if(marks >= 80 && marks < 100){
+                        return <h2>A +</h2>
+                    }
+                }
+            )()}
         </div>
     );
 };
