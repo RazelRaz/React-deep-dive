1687fb4048fa1ff85e242552e345203808e679c0
RazelRaz <razel16dcm@gmail.com>
2023-07-09 16:12:17 +0600

    JSX Short Hand If else

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,13 @@
 
 
 const Demo = () => {
+    let marks = 80;
     return (
         <div>
             <h1>Im Demo</h1>
+            {
+                marks > 80 ? <h2>A Grade</h2> : <p>Below 80</p>
+            }
         </div>
     );
 };
